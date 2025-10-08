b167218105b9df4e6a7828d192f6ddd3805670b9
Alisson Oliveira <alisson.dev.py@gmail.com>
2025-10-08 19:10:40 -0300

    style: padrão do estilo criado

--- src/components/registrationForm.tsx
+++ src/components/registrationForm.tsx
@@ -20,18 +20,24 @@ const RegistrationForm = () => {
     };
 
     return (
-        <form onSubmit={handleSubmit(onSubmit)}>
-            <div>
+        <form 
+            onSubmit={handleSubmit(onSubmit)}
+            className="w-96 bg-white p-4 rounded-md border border-slate-300"
+        >
+            <div className="flex flex-col gap-2 mb-2">
                 <label htmlFor="name">Nome:</label>
                 <input 
                     type="text" 
                     id="name" 
                     placeholder="Digite o seu nome." 
-                    {...register("name")} 
+                    {...register("name")}
+                    className="h-10 pl-2 rounded-md border border-slate-300"
                 />
-                {errors.name && (<small style={{ color: 'red' }}>{errors.name.message}</small>)}
+                {errors.name && (
+                    <small className="text-red-500 italic" style={{ color: 'red' }}>{errors.name.message}</small>
+                )}
             </div>
-            <button type="submit">Cadastrar</button>
+            <button type="submit" className="w-full h-11 bg-cyan-400 rounded-md my-2 hover:bg-cyan-500 transition-all">Cadastrar</button>
         </form>
     );
 };
